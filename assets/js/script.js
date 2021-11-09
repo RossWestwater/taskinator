8b6488e91f5dfddc60a914482916b9e993d01f05
Ross Westwater <ross.westwater@gmail.com>
2021-11-09 12:58:14 -0700

    added ability to change tasks to various stages of work status

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -41,9 +43,9 @@ var taskFormHandler = function(event) {
 };
 
 var completeEditTask = function(taskName, taskType, taskId) {
-  var taskSelected = document.querySelector("task-item[data-task-id='" + taskId + "']");
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
 
-  taskSelected.querySelector("h3.task-name").textContent = taskName;
+  taskSelected.querySelector("h3.task-name").textContent = taskName;  
   taskSelected.querySelector("span.task-type").textContent = taskType;
 
   alert("Task Updated!");
@@ -145,5 +147,22 @@ var deleteTask = function(taskId) {
   taskSelected.remove();
 };
 
+var taskStatusChangeHandler = function(event) {
+  var taskId = event.target.getAttribute("data-task-id");
+  var statusValue = event.target.value.toLowerCase();
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+  if (statusValue === "to do") {
+    tasksToDoEl.appendChild(taskSelected);
+  } 
+  else if (statusValue === "in progress") {
+    tasksInProgressEl.appendChild(taskSelected);
+  } 
+  else if (statusValue === "completed") {
+    tasksCompletedEl.appendChild(taskSelected);
+  }
+};
+
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
